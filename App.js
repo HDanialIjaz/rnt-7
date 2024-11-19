fcdd03e63c347485fef2133a04ca28737db61a9e
Hafiz Muhammad Danial <danialijaz098@gmail.com>
2024-11-19 21:47:56 +0500

    Initial commit

---
+++ App.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import { Text, View, Image, StyleSheet,TouchableOpacity } from 'react-native';
+
+const ProductCard = () => {
+  return (
+    <View style={style.contanair}>
+      <View>
+        <Image
+          source={{
+            uri: 'https://cdn.shopify.com/s/files/1/0066/0360/4086/products/84176806f3844d4289036491be7a63f9_61962a12-1613-4a13-8c5e-aca379b8ae3a_600x.jpg?v=1663658145',
+          }}
+          style={style.img}
+        />
+      </View>
+      <View style={style.info_container}>
+        <View style={style.one_line}>
+          <Text style={style.title}>Classic Utility Jacket</Text>
+          <Text style={style.price}>$110.00</Text>
+        </View>
+        <Text style={style.stock}>In stock</Text>
+      </View>
+    </View>
+  );
+};
+export default ProductCard;
+
+const style = StyleSheet.create({
+  contanair: {
+    backgroundColor: '#fff',
+    borderRadius: 8,
+    overflow: 'hidden',
+    margin: 16,
+    elevation: 5,
+  },
+  img: {
+    width: '100%',
+    height: 280,
+    resizeMode: 'cover',
+  },
+  info_container: {
+    padding: 10,
+  },
+  one_line:{
+    flexDirection:'row',
+    justifyContent:'space-between'
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  price: {
+    fontSize: 15,
+    color: '#555',
+    padding: 0,
+    marginTop: 5,
+  },
+  stock: {
+    fontSize: 15,
+    color: '#AF504C',
+    marginTop: 5,
+  },
+    
+});
